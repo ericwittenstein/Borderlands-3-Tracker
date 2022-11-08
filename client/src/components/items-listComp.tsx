929955f6d2a7faa6ea2c4f9e517dc0aa6a720346
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-08 13:13:34 -0500

    added the reder function that should display the list of items and the actively selected item

--- client/src/components/items-listComp.tsx
+++ client/src/components/items-listComp.tsx
@@ -17,7 +17,7 @@ type State = {
 export default class ItemsList extends Component<Props, State> {
 	constructor(props: Props) {
 		super(props);
-        // bind the change events to the item props
+		// bind the change events to the item props
 		this.onChangeSearchName = this.onChangeSearchName.bind(this);
 		this.retrieveItems = this.retrieveItems.bind(this);
 		this.refreshList = this.refreshList.bind(this);
@@ -25,7 +25,7 @@ export default class ItemsList extends Component<Props, State> {
 		this.removeAllItems = this.removeAllItems.bind(this);
 		this.searchName = this.searchName.bind(this);
 
-        // initial state of the component
+		// initial state of the component
 		this.state = {
 			items: [],
 			currentItem: null,
@@ -34,12 +34,12 @@ export default class ItemsList extends Component<Props, State> {
 		};
 	}
 
-    // if component mounted successfully, retrieve the list of items
+	// if component mounted successfully, retrieve the list of items
 	componentDidMount() {
 		this.retrieveItems();
 	}
 
-    // tracker for value changes and assignment for search bar
+	// tracker for value changes and assignment for search bar
 	onChangeSearchName(e: ChangeEvent<HTMLInputElement>) {
 		const searchName = e.target.value;
 
@@ -48,7 +48,7 @@ export default class ItemsList extends Component<Props, State> {
 		});
 	}
 
-    // function to retrieve list of items
+	// function to retrieve list of items
 	retrieveItems() {
 		ItemDataService.findAll()
 			.then((response) => {
@@ -62,52 +62,167 @@ export default class ItemsList extends Component<Props, State> {
 			});
 	}
 
-    // function to refresh and reset the item list display
+	// function to refresh and reset the item list display
 	refreshList() {
-        this.retrieveItems();
-        this.setState({
-            currentItem: null,
-            currentIndex: -1
-        });
-    }
+		this.retrieveItems();
+		this.setState({
+			currentItem: null,
+			currentIndex: -1
+		});
+	}
 
-    // function to designate the current item as active
-    setActiveItem(item: ItemData, index: number){
-        this.setState({
-            currentItem: item,
-            currentIndex: index
-        });
-    }
+	// function to designate the current item as active
+	setActiveItem(item: ItemData, index: number) {
+		this.setState({
+			currentItem: item,
+			currentIndex: index
+		});
+	}
 
-    // function to delete all items
-    // DO NOT USE UNLESS ABSOLUTELY SURE
-    removeAllItems(){
-        ItemDataService.deleteAll().then((response: any) => {
-            console.log(response.data);
-            this.refreshList();
-        }).catch((e: Error)=> {
-            console.log(e);
-        });
-    }
+	// function to delete all items
+	// DO NOT USE UNLESS ABSOLUTELY SURE
+	removeAllItems() {
+		ItemDataService.deleteAll()
+			.then((response: any) => {
+				console.log(response.data);
+				this.refreshList();
+			})
+			.catch((e: Error) => {
+				console.log(e);
+			});
+	}
 
-    // function to handle the search by name feature
-    searchName(){
-        this.setState({
-            currentItem: null,
-            currentIndex: -1
-        });
+	// function to handle the search by name feature
+	searchName() {
+		this.setState({
+			currentItem: null,
+			currentIndex: -1
+		});
 
-        ItemDataService.findByName(this.state.searchName).then((response: any)=> {
-            this.setState({
-                items: response.data
-            });
-            console.log(response.data);
-        }).catch((e: Error) => {
-            console.log(e);
-        });
-    }
+		ItemDataService.findByName(this.state.searchName)
+			.then((response: any) => {
+				this.setState({
+					items: response.data
+				});
+				console.log(response.data);
+			})
+			.catch((e: Error) => {
+				console.log(e);
+			});
+	}
 
-    // render() {
+	render() {
+		const { searchName, items, currentItem, currentIndex } = this.state;
 
-    // }
+		return (
+			<div className="list row">
+				<div className="col-md-8">
+					<div className="input-group mb-3">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Search by Item Name"
+							value={searchName}
+							onChange={this.onChangeSearchName}
+						/>
+						<div className="input-group-append">
+							<button
+								className="btn btn-outline-secondary"
+								type="button"
+								onClick={this.searchName}
+							>
+								Search
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-6">
+					<h4>Items List</h4>
+
+					<ul className="list-group">
+						{items &&
+							items.map((item: ItemData, index: number) => (
+								<li
+									className={
+										"list-group-item " +
+										(index === currentIndex ? "active" : "")
+									}
+									onClick={() =>
+										this.setActiveItem(item, index)
+									}
+									key={index}
+								>
+									{item.name}
+								</li>
+							))}
+					</ul>
+					{/* THIS IS THE DELETE ALL BUTTON, DO NOT USE UNLESS ABSOLUTELY NECESSARY */}
+					{/* <button
+						className="m-3 btn btn-sm btn-danger"
+						onClick={this.removeAllItems}
+					>
+						Remove All
+					</button> */}
+				</div>
+				<div className="col-md-6">
+					{currentItem ? (
+						<div>
+							<h4>Item</h4>
+							<div>
+								<label>
+									<strong>Name:</strong>
+								</label>{" "}
+								{currentItem.name}
+							</div>
+							<div>
+								<label>
+									<strong>Item Type:</strong>
+								</label>{" "}
+								{currentItem.item_type}
+							</div>
+							<div>
+								<label>
+									<strong>Element:</strong>
+								</label>{" "}
+								{currentItem.element}
+							</div>
+							<div>
+								<label>
+									<strong>Effect:</strong>
+								</label>{" "}
+								{currentItem.effect}
+							</div>
+							<div>
+								<label>
+									<strong>Notes:</strong>
+								</label>{" "}
+								{currentItem.notes}
+							</div>
+							<div>
+								<label>
+									<strong>Recommended:</strong>
+								</label>{" "}
+								{currentItem.recommended}
+							</div>
+
+							<Link
+								to={"/items/" + currentItem.id}
+								className="badge badge-warning"
+							>
+								Edit
+							</Link>
+						</div>
+					) : (
+						<div>
+							<br />
+							<p>
+								Please click on an item to pull up its
+								information sheet
+							</p>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
