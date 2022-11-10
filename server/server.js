bd18cf5bca5c75b5f28039a018ab131a421eb502
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-10 12:34:59 -0500

    working on heroku deployment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,12 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // configure express routing to point to correct folder
-app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.get("/*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+	});
+}
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -30,9 +35,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.get("/", (req, res) => {
 // 	res.json({ message: "Welcome!" });
 // });
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
 
 app.use(routes);
 
