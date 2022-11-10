c4cd1e7dc1dc8615bf39ac04b722065b4031d832
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-10 14:22:07 -0500

    working on full functionality on heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 var corsOptions = {
-	origin: "https://bl3tracker.herokuapp.com/",
+	origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
