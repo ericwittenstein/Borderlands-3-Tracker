bb7d690f2cf0adfd3c20935182b2572166dbee76
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-10 15:23:39 -0500

    new controllers written

--- server/controllers/item.controller.js
+++ server/controllers/item.controller.js
@@ -19,29 +19,121 @@ exports.create = (req, res) => {
 		element: req.body.element,
 		effect: req.body.effect,
 		notes: req.body.notes,
-		recommended: req.body.recommended ? req.body.recommended : false
+		recommended: req.body.recommended ? req.body.recommended : false,
 	};
 
-    Item.create(item).then(data => {
-        res.status(200).send(data);
-    }).catch (err => {
-        res.status(500).send({message: `Something prevented the item from being created: ${err}`});
-    });
+	Item.create(item)
+		.then((data) => {
+			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: `Something prevented the item from being created: ${err}`,
+			});
+		});
 };
 
 // retrieve all items from db
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+	const name = req.query.name;
+	var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
+
+	Item.findAll({ where: condition })
+		.then((data) => {
+			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: `Something went wrong while trying to retrieve items: ${err}`,
+			});
+		});
+};
 
 // find one item by id
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+	const id = req.params.id;
+
+	Item.findByPk(id)
+		.then((data) => {
+			if (data) {
+				res.status(200).send(data);
+			} else {
+				res.status(404).send({
+					message: `Cannot find item at id=${id}`,
+				});
+			}
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: `Something went wrong while tring to retrieve item id=${id}: ${err}`,
+			});
+		});
+};
 
 // update item by id
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+	const id = req.params.id;
+
+	Item.update(req.body, {
+		where: { id: id },
+	})
+		.then((num) => {
+			if (num == 1) {
+				res.status(200).send({ message: "Item Successfully Updated" });
+			} else {
+				res.status(404).send({ message: "Was Not Updated" });
+			}
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: `Something went wrong during the update: ${err}`,
+			});
+		});
+};
 
 // delete item by id
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+	const id = req.params.id;
+
+	Item.destroy({
+		where: { id: id },
+	})
+		.then((num) => {
+			if (num == 1) {
+				res.status(200).send({ message: "Successfully Deleted" });
+			} else {
+				res.status(404).send({ message: "Was Not Deleted" });
+			}
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: `Something went wrong during the deletion process: ${err}`,
+			});
+		});
+};
 
 // delete all items
-exports.deleteAll = (req, res) => {};
+exports.deleteAll = (req, res) => {
+	Item.destroy({
+		where: {},
+		truncate: false,
+	})
+		.then((nums) => {
+			res.status(200).send({ message: `${nums} Items were deleted!` });
+		})
+		.catch((err) => {
+			res.status(500).send({ message: `Something went wrong: ${err}` });
+		});
+};
 
-exports.findAllRecommended = (req, res) => {};
+exports.findAllRecommended = (req, res) => {
+	Item.findAll({ where: { recommended: true } })
+		.then((data) => {
+			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: `Something went wrong while searching: ${err}`,
+			});
+		});
+};
