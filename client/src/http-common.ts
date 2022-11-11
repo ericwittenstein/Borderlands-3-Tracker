d2e7555377c100037669562022498c43d45b8621
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-10 20:42:53 -0500

    working on jaws db

--- client/src/http-common.ts
+++ client/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	url: "/api/items/",
+	url: "https://bl3tracker.herokuapp.com//api/items/",
 	headers: {
 		"Content-type": "application/json"
 	}
