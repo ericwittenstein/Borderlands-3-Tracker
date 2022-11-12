dae6767f8b76ab465d771c28f8bc11f459a2f412
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-11-12 15:26:44 -0500

    working on getting routes to work still

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 var corsOptions = {
-	origin: "http://localhost:8081",
+	origin: "https://bl3tracker.herokuapp.com/",
 };
 
 app.use(cors(corsOptions));
